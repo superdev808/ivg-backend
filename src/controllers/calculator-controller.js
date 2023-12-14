9a1aae40b3741ef9e6bfeb0b8917d8b238ded8e1
dev <dev@leo.local>
2023-12-14 01:30:57 -0800

    [IV-64]: Implementation of Calculators Api

---
+++ src/controllers/calculator-controller.js
@@ -0,0 +1,46 @@
+const CrownMaterialModel = require("../models/crown-material-model");
+const ScanbodyModel = require("../models/scanbody-model");
+const response = require("../utils/response");
+const _ = require("lodash");
+
+exports.getCalculatorOptions = async (req, res, next) => {
+    const { type, quiz, fields } = req.body;
+
+    const calculatorType = decodeURIComponent(type);
+
+    let Model = null;
+
+    switch (calculatorType) {
+        case "Scanbodies":
+            Model = ScanbodyModel;
+            break;
+        case "Crown Materials":
+            Model = CrownMaterialModel;
+            break;
+        default:
+            break;
+    }
+
+    if (!Model) {
+        response.notFoundError(res, `${type} data is not existing`);
+        return;
+    }
+
+    const query = quiz;
+    const data = await Model.find(query);
+    let result = [];
+    if (fields.length > 1) {
+        result = _.uniq(data.map((item) => {
+            const res = {};
+            fields.forEach(field => {
+                res[field] = item[field];
+            });
+
+            return res;
+        }));
+    } else {
+        result = _.uniq(data.map((item) => item[fields[0]]));
+    }
+
+    response.success(res, result);
+}
