c5e54f4a6d0dddec45266a877bb2b0f3ebbc712f
superdev808 <drose6307@gmail.com>
2024-02-08 06:08:27 -0800

    [IV-130]: Data Collection update for single calculators

--- src/models/bone-reduction-model.js
+++ src/models/bone-reduction-model.js
@@ -2,19 +2,21 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const schema = new Schema({
-    "Implant Brand": { type: String },
-    "Implant Model": { type: String },
-    "Implant Diameter": { type: String },
-    "Implant Platform": { type: String },
-    "Will you perform bone reduction?": { type: String },
-    "Bur Kit Name (Bone Reduction)": { type: String },
-    "Item Number": { type: String },
-    "Link to Purchase": { type: String },
-    "Bur Kit (Denture Conversion) Name": { type: String },
-    "Bur Link to Purchase": { type: String }
+  "Implant Brand": { type: String },
+  "Implant Model": { type: String },
+  "Implant Diameter": { type: String },
+  "Implant Platform": { type: String },
+  "Bur Kit Name (Bone Reduction)": { type: String },
+  "Item Number": { type: String },
+  "Bur Kit (Bone Reduction) Link to Purchase": { type: String },
+  "Bur Kit (Denture Conversion) Name": { type: String },
+  "Bur Kit (Denture Conversion) Link to Purchase": { type: String },
 });
 
-const BoneReductionModel = mongoose.model('BoneReduction', schema, "Surgery_BoneReduction");
+const BoneReductionModel = mongoose.model(
+  "BoneReduction",
+  schema,
+  "Surgery_BoneReduction"
+);
 
 module.exports = BoneReductionModel;
-
