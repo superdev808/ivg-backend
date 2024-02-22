1a8ad65ba936faf81b79e4edaa0322824507bc74
kenethhu <kenethhu.dev@gmail.com>
2024-02-21 22:55:08 -0800

    added new domains to origin.

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
 
-require("dotenv").config({ path: path.resolve(__dirname, "./../.env") });
+require('dotenv').config({ path: path.resolve(__dirname, './../.env') });
 
-const AppRoutes = require("./routes/app-routes");
-const CalculatorRoutes = require("./routes/calculator-routes");
-const WorkflowRoutes = require("./routes/workflow-routes");
+const AppRoutes = require('./routes/app-routes');
+const CalculatorRoutes = require('./routes/calculator-routes');
+const WorkflowRoutes = require('./routes/workflow-routes');
 const UserRoutes = require('./routes/user-routes');
 const PublicRoutes = require('./routes/public-routes');
 
@@ -16,32 +16,32 @@ const app = express();
 
 app.use(helmet());
 app.use(
-    helmet.hsts({
-        maxAge: 63072000, // HSTS set to 2 years (default by Mozilla)
-        includeSubDomains: true
-    })
+	helmet.hsts({
+		maxAge: 63072000, // HSTS set to 2 years (default by Mozilla)
+		includeSubDomains: true,
+	})
 );
 app.use(
-    helmet.contentSecurityPolicy({
-        directives: {
-            "connect-src": ["'self'"],
-            "default-src": ["none"],
-            "frame-ancestors": ["none"]
-        }
-    })
+	helmet.contentSecurityPolicy({
+		directives: {
+			'connect-src': ["'self'"],
+			'default-src': ['none'],
+			'frame-ancestors': ['none'],
+		},
+	})
 );
 app.use(
-    helmet.frameguard({
-        action: "deny"
-    })
+	helmet.frameguard({
+		action: 'deny',
+	})
 );
 
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 const router = express.Router();
 const port = process.env.PORT || 8000;
-app.set("port", port);
+app.set('port', port);
 
 AppRoutes.getRoutes(app, router);
 CalculatorRoutes.getRoutes(app, router);
@@ -50,33 +50,39 @@ UserRoutes.getRoutes(app, router);
 PublicRoutes.getRoutes(app, router);
 
 app.use(
-    "/v1/",
-    cors({
-        optionsSuccessStatus: 200,
-        credentials: true,
-        origin: ["https://ig-dev.zensol.app","http://localhost:3000"]
-    }),
-    router
+	'/v1/',
+	cors({
+		optionsSuccessStatus: 200,
+		credentials: true,
+		origin: [
+			'https://ig-dev.zensol.app',
+			'http://localhost:3000',
+			'https://igdevelopment.app',
+			'https://ivoryguide.com',
+			'https://staging.ivoryguide.com',
+		],
+	}),
+	router
 );
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-if (process.env.CURRENT_ENV !== "prod") {
-    mongoose.set("debug", true);
-    mongoose.set("debug", { color: false });
+if (process.env.CURRENT_ENV !== 'prod') {
+	mongoose.set('debug', true);
+	mongoose.set('debug', { color: false });
 }
 
-console.log(process.env.MongoDbUri)
+console.log(process.env.MongoDbUri);
 mongoose.connect(process.env.MongoDbUri).then(
-    () => {
-        console.log("Connected to MongoDB");
-    },
-    async err => {
-        console.error(err);
-        return;
-    }
+	() => {
+		console.log('Connected to MongoDB');
+	},
+	async (err) => {
+		console.error(err);
+		return;
+	}
 );
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
